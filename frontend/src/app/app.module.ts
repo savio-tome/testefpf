00c6776b7d13bb6c4e0db356df38b3dbdf1d40c5
savio.tome <savio@akirymedia.com.br>
2022-03-24 19:18:07 -0400

    create header, sidenav components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+//components
+import { HeaderComponent } from './mainpage/header/header.component';
+import { SidenavComponent } from './mainpage/sidenav/sidenav.component'
+
+//imports angular/material
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list'
+import {MatSidenavModule} from '@angular/material/sidenav'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
